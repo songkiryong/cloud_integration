7d61df571fe9374db1457bab01f2bd6d6c92a6a2
songkiryong <73922068+songkiryong@users.noreply.github.com>
2021-09-11 01:38:02 +0900

    Add files via upload

---
+++ requirements.sh
@@ -0,0 +1,7 @@
+git clone https://github.com/ultralytics/yolov5.git
+pip install flask-ngrok
+pip install django
+pip install postgresql
+sudo apt install tesseract-ocr
+pip install pytesseract
+pip install -r ./yolov5/requirements.txt
